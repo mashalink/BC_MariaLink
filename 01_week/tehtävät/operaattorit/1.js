707359438751a4ba65a032af9934799b36c38248
Maria <maria8link@gmail.com>
2025-03-31 10:06:32 +0300

    all the tasks of the first week have been completed

---
+++ 01_week/tehtävät/operaattorit/1.js
@@ -0,0 +1,32 @@
+/* 
+- Luo muuttujat a ja b ja anna niille kokonaislukuarvot.
+- Käytä yhteen-, vähennys-, kerto- ja jakolaskuoperaattoreita muuttujille a ja b. Tallenna jokainen tulos erilliseen muuttujaan (summa, erotus, tulo, osamäärä, jakojäännös).
+- Käytä console.log() -metodia tulostamaan jokaisen operaation tulos konsoliin.
+- Lisää kommentteja koodiin selittämään, mitä kukin operaatio tekee.
+*/
+
+// Määritellään kaksi kokonaislukumuuttujaa 'a' ja 'b'
+let a = 10;
+let b = 3;
+
+// Lasketaan summa ja tallennetaan se muuttujaan 'summa'
+let summa = a + b;
+
+// Lasketaan erotus ja tallennetaan se muuttujaan 'erotus'
+let erotus = a - b;
+
+// Lasketaan tulo ja tallennetaan se muuttujaan 'tulo'
+let tulo = a * b;
+
+// Lasketaan osamäärä ja tallennetaan se muuttujaan 'osamäärä'
+let osamäärä = a / b;
+
+// Lasketaan jakojäännös ja tallennetaan se muuttujaan 'jakojäännös'
+let jakojäännös = a % b;
+
+// Tulostetaan kaikki tulokset konsoliin
+console.log("Summa:", summa);
+console.log("Erotus:", erotus);
+console.log("Tulo:", tulo);
+console.log("Osamäärä:", osamäärä);
+console.log("Jakojäännös:", jakojäännös);
---
+++ 01_week/tehtävät/operaattorit/2.js
@@ -0,0 +1,13 @@
+/**Tehtävä: Merkkijonojen yhdistäminen
+ * Luo kaksi muuttujaa, etunimi ja sukunimi, ja aseta niihin omat etu- ja sukunimesi.
+ * Yhdistä nämä kaksi merkkijonoa ja tulosta tulos konsoliin.*/
+
+// Määritellään muuttujat etunimi ja sukunimi
+let etunimi = "Matti";
+let sukunimi = "Kuummola";
+
+// Yhdistetään merkkijonot ja tallennetaan tulos muuttujaan 'kokoNimi'
+let kokoNimi = etunimi + " " + sukunimi;
+
+// Tulostetaan yhdistetty merkkijono konsoliin
+console.log(kokoNimi);
