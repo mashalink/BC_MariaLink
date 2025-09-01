bbc9c2bd0341897e4b598da18e3f2d47dcc40599
Maria <maria8link@gmail.com>
2025-09-01 09:54:27 +0300

    WIP

--- SQL/ekadb.sql
+++ SQL/ekadb.sql
@@ -1,70 +1,56 @@
-sudo mysql -u root
+mysql -u root -p
 SHOW DATABASES;
-CREATE DATABASE ekadb;
+CREATE DATABASE ekadb; / CREATE DATABASE IF NOT EXISTS ekadb;
 SHOW DATABASES;
 USE ekadb
 SHOW TABLES;
-create table testi( 
-    -> id integer not null primary key,
-    -> nimi VARCHAR(30) not null
-    -> );
+CREATE TABLE IF NOT EXISTS testi(
+  id   INT NOT NULL PRIMARY KEY,
+  nimi VARCHAR(30) NOT NULL
+);
 SHOW TABLES;
-insert into testi (id, nimi) values(1, 'Leila');
-select * from testi;
-insert into testi (id, nimi) values(2, 'Matti');
-select nimi from testi;
-select nimi, id from testi;
-describe testi;
-show create table testi;
+INSERT INTO testi (id, nimi) VALUES (1,'Leila');
+SELECT * FROM testi;
+INSERT INTO testi (id, nimi) VALUES (2,'Matti');
+SELECT * FROM testi;
+SELECT nimi,id FROM testi;
+SELECT id,nimi FROM testi;
+DESCRIBE testi;
+SHOW CREATE TABLE testi;
+
 CREATE USER 'secret'@'localhost' IDENTIFIED BY 'secret';
 SELECT User, Host FROM mysql.user;
 DROP USER 'secret'@'localhost';
-create user 'admin1'@'localhost' identified by '12345';
-grant all privileges on *.* to 'admin1'@'localhost' with grant option;
-SELECT User, Host, password FROM mysql.user where user='admin1';
-mysql -u admin1 -p
-show databases;
-use ekadb;
-select * from testi;
-drop database ekadb;
 
+CREATE USER IF NOT EXISTS 'admin1'@'localhost' IDENTIFIED BY '12345';
+GRANT ALL PRIVILEGES ON *.* TO 'admin1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+-- select user,host,password from mysql.user;
+-- смотреть пароль в mysql.user не надо (эта колонка устарела).
+-- лучше так:
+SELECT User,Host,plugin FROM mysql.user WHERE User='admin1';
+EXIT;
 
-create user 'matti'@'localhost' identified by 'salainen';
-grant select, insert on ekadb.testi to 'matti'@'localhost';
-exit;
+mysql -u admin1 -p -e "SHOW DATABASES;"
+mysql -u admin1 -p -e "SELECT * FROM ekadb.testi;"
+
+CREATE USER IF NOT EXISTS 'matti'@'localhost' IDENTIFIED BY 'salainen';
+GRANT SELECT, INSERT ON ekadb.testi TO 'matti'@'localhost';
+FLUSH PRIVILEGES;
+
+SHOW GRANTS FOR 'matti'@'localhost';
+EXIT;
+
 mysql -u matti -p
-show databases;
-use ekadb;
-select * from testi;
-insert into testi (id, nimi) values(30, 'Verra');
-elete from testi;
-ERROR 1142 (42000): DELETE command denied to user 'matti'@'localhost' for table `ekadb`.`testi`
-MariaDB [ekadb]> drop databases ekadb;
-ERROR 1064 (42000): You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'databases ekadb' at line 1
-exit;
-
-mysql -u admin1 -p
-Enter password: 
-Welcome to the MariaDB monitor.  Commands end with ; or \g.
-Your MariaDB connection id is 10
-Server version: 12.0.2-MariaDB Homebrew
-
-Copyright (c) 2000, 2018, Oracle, MariaDB Corporation Ab and others.
-
-Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
-
-MariaDB [(none)]> show databases;
-+--------------------+
-| Database           |
-+--------------------+
-| ekadb              |
-| information_schema |
-| mysql              |
-| performance_schema |
-| sys                |
-+--------------------+
-5 rows in set (0.002 sec)
-
-MariaDB [(none)]> create databases ekadb;
-ERROR 1064 (42000): You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'databases ekadb' at line 1
-MariaDB [(none)]> 
+SHOW DATABASES;  
+USE ekadb;
+SELECT * FROM testi;  
+INSERT INTO testi(id,nimi) VALUES (3,'Pirkko');
+SELECT * FROM testi;
+INSERT INTO testi(id,nimi) VALUES (30,'Veera');
+SELECT * FROM testi;
+-- дальше ДОЛЖНО падать (нет прав):
+-- DELETE FROM testi;
+-- DROP DATABASE ekadb;
+-- DROP TABLE testi;
+EXIT;
